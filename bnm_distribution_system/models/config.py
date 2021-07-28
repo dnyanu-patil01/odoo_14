cccca24aeacb92662c22ad6bdd7a72cd0f9112bd
leelapriskila <leelapriskila@msystechnologies.com>
2021-07-28 22:29:00 +0530

    Added BNM Distribution System Module

---
+++ bnm_distribution_system/models/config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class website(models.Model):
+
+    _inherit = 'website'
+
+    checkout_skip_payment = fields.Boolean(default=False, readonly=False)
+
+class ResConfigSettings(models.TransientModel):
+
+    _inherit = 'res.config.settings'
+
+    checkout_skip_payment = fields.Boolean(
+        related='website_id.checkout_skip_payment',
+        string="Skip Payment In Checkout", readonly=False)
