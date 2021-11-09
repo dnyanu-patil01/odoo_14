9dc4feb67dd65eb2b4ed77e19e63ee622431854c
Leelapriskila <leelapriskila@msystechnologies.com>
2021-11-09 11:24:31 +0530

    Invoice Sequence Fixes

--- seller_management/models/account_move.py
+++ seller_management/models/account_move.py
@@ -13,3 +13,13 @@ class AccountMove(models.Model):
 
     def print_invoice(self):
         return self.env.ref('account.account_invoices').report_action(self)
+    
+    #override _get_last_sequence_domain to fix sequence without seller
+    def _get_last_sequence_domain(self, relaxed=False):
+        where_string, param = super(AccountMove, self)._get_last_sequence_domain(relaxed)
+        if self.seller_id:
+            where_string += "AND seller_id = %(seller_id)s"
+            param.update({'seller_id':self.seller_id.id})
+        else:
+            where_string += "AND seller_id is null"
+        return where_string, param
