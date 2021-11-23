35719640f5260d241b3f18850728662aa2d81ead
Leelapriskila <leelapriskila@msystechnologies.com>
2021-11-23 20:48:49 +0530

    Fixes Access Rights & Record Rules For Sellers While Creating Backorders And Returns Orders

--- seller_management/models/stock_picking.py
+++ seller_management/models/stock_picking.py
@@ -4,3 +4,12 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     seller_id = fields.Many2one("res.partner",string="Seller",copy=False,domain="[('seller','=',True),('active','=',True),('parent_id','=',False)]")
+
+    @api.model
+    def create(self, vals):
+        #To Pass Seller ID While Creating Backorders
+        if 'backorder_id' in vals and vals['backorder_id'] != False:
+            orgin = self.browse(int(vals['backorder_id']))
+            if orgin.seller_id:
+                vals.update({'seller_id':orgin.seller_id.id})
+        return super(StockPicking ,self).create(vals)
