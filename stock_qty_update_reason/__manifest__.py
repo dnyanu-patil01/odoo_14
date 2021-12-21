e2fd7349ee48f0ec6caa56fcd946b95d8a0e6ec9
Leelapriskila <leelapriskila@msystechnologies.com>
2021-12-21 16:39:30 +0530

    Added Reason While Updating Product Quantity

---
+++ stock_qty_update_reason/__manifest__.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+
+{
+    'name': 'Stock Update Reason',
+    'version': '14.0',
+    'author': '',
+    'website': '',
+    'license': 'AGPL-3',
+    'category': 'Product',
+    'summary': "Added Reason Text Field To Capture The Reason For Stock Update",
+    'depends': [
+                'product','stock'
+                ],
+    'data': ['views/view.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'demo': [],
+    'test': []
+}
