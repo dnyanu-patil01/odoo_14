53d07305bf1ed563ac7bfa03693f24b30cc22d49
Leelapriskila <leelapriskila@msystechnologies.com>
2022-08-16 18:28:34 +0530

    Added Excel Report For Sellers

---
+++ seller_excel_report/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Excel Report For Sellers',
+    'category': 'Report',
+    'summary': 'Excel Report For Sellers',
+    'author': 'leelapriskila',
+    'version': '14.0',
+    'description': """
+    Sale Excel Report,Invoice Excel Report
+    """,
+    'depends': ['sale','seller_management'],
+    'data': [
+        "security/ir.model.access.csv",
+        'wizard/sale_report_wizard_view.xml',
+        "wizard/invoice_report_wizard_view.xml"
+    ],
+    'qweb': [],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
