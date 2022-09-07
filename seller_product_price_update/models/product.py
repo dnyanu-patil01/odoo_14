79110d140645736e32d63cf3b6b79e2bd920578c
Leelapriskila <leelapriskila@msystechnologies.com>
2022-09-07 17:52:56 +0530

    Seller Price Update Issue Fixed

--- seller_product_price_update/models/product.py
+++ seller_product_price_update/models/product.py
@@ -15,3 +15,31 @@ class ProductTemplate(models.Model):
             default_product_tmpl_id=self.id
         )
         return action
+
+
+class ProductPricelist(models.Model):
+    _inherit = 'product.pricelist.item'
+
+    def create(self,values):
+        product = super(ProductPricelist,self).create(values)
+        if self.env.context.get('update_shopify_price'):
+            product.push_price_change_to_shopify()
+        return product
+
+    def write(self,values):
+        product = super(ProductPricelist,self).write(values)
+        if self.env.context.get('update_shopify_price'):
+            self.push_price_change_to_shopify()
+        return product
+        
+    def push_price_change_to_shopify(self):
+        shopify_product_tmpl_id = self.env['shopify.product.template.ept'].search([('product_tmpl_id','=',self.product_tmpl_id.id)])
+        if shopify_product_tmpl_id:
+            import_product_obj = self.env['shopify.process.import.export'].create({
+            'shopify_is_update_basic_detail':False,
+            'shopify_is_set_price':True,
+            'shopify_is_set_image':False,
+            'shopify_is_publish':'publish_product_web',
+            })
+            import_product_obj.with_context({'active_ids':shopify_product_tmpl_id.id}).sudo().manual_update_product_to_shopify() 
+        return True   
