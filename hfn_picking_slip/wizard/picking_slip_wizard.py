0cf0885782821ac6247866b6141657f947a702e6
Leelapriskila <leelapriskila@msystechnologies.com>
2022-10-12 14:12:03 +0530

    Added Pickup Slip Report In DO

--- hfn_picking_slip/wizard/picking_slip_wizard.py
+++ hfn_picking_slip/wizard/picking_slip_wizard.py
@@ -5,9 +5,14 @@ class PickingSlipWizard(models.TransientModel):
     _name = 'picking.slip.wizard'
     _description="Wizard To Print Picking Slip"
 
+    start_date = fields.Date('Start Date',required=True)
+    end_date = fields.Date('End Date',required=True)
+
     def print_pickup_slip(self):
         data={
-            'wizard_id':self.id
+            'wizard_id':self.id,
+            'start_date':self.start_date,
+            'end_date':self.end_date,
         }
         return self.env.ref('hfn_picking_slip.action_report_picking_slip').report_action(self, data=data)
 
