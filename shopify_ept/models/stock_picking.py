b7f44693797c49a9fc182823475d232ee9443874
LakshmanaSastryHeartfulness <gorthivekata.laxmanasastry@volunteer.heartfulness.org>
2024-04-06 12:43:15 +0530

    [IMP] shpify_ept : Added Shopify Order number in delivery order(Transfers)-HFN Life

--- shopify_ept/models/stock_picking.py
+++ shopify_ept/models/stock_picking.py
@@ -9,3 +9,4 @@ class StockPicking(models.Model):
     updated_in_shopify = fields.Boolean(default=False)
     is_shopify_delivery_order = fields.Boolean("Shopify Delivery Order", default=False)
     shopify_instance_id = fields.Many2one("shopify.instance.ept", "Instance")
+    shopify_order_number = fields.Char(related="sale_id.shopify_order_number", string="Shopify Order Number", store=True)
