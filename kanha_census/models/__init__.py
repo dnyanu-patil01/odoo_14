9c520572724d3cba107d1829c68b457f7fd58f7c
dnyanubelokar <nano.nano.belokar02@gmail.com>
2025-08-11 17:19:08 +0530

    updated

--- kanha_census/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_partner
-from . import kanha_location
-from . import vehicle_details
-from . import residents_documents_downloads_history
-from . import work_profile
-from . import res_users
